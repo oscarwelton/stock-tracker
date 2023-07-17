75d3ad68950662b839f026ebe14652ffd295d4b0
oscar welton <oscarwelton@gmail.com>
2023-07-17 20:29:30 +0100

    added date variation buttons

--- frontend/src/widgets/Chart.jsx
+++ frontend/src/widgets/Chart.jsx
@@ -1,19 +1,18 @@
 import React from "react";
 import { useEffect } from "react";
 import { formatDate } from "helpers/date-helper";
-import {
-  AreaChart,
-  Area,
-  XAxis,
-  YAxis,
-  Tooltip,
-} from "recharts";
+import { AreaChart, Area, XAxis, YAxis, Tooltip } from "recharts";
 
-const Chart = ( chartData ) => {
+const Chart = (chartData) => {
   console.log(chartData);
 
   const formatData = () => {
-    if (chartData && chartData.chart && chartData.chart.c && chartData.chart.t) {
+    if (
+      chartData &&
+      chartData.chart &&
+      chartData.chart.c &&
+      chartData.chart.t
+    ) {
       return chartData.chart.c.map((point, index) => ({
         value: point.toFixed(2),
         date: formatDate(chartData.chart.t[index]),
@@ -23,12 +22,22 @@ const Chart = ( chartData ) => {
   };
 
   return (
-    <AreaChart data={formatData(chartData)} height={300} width={500}>
-      <Area type="monotone" dataKey="value" stroke="#8884d8" fill="#8884d8" />
-      <XAxis dataKey="date" />
-      <YAxis domain={["dataMin", "dataMax"]} />
-      <Tooltip />
-    </AreaChart>
+    <>
+      <div>
+        <button>24h</button>
+        <button>7d</button>
+        <button>1m</button>
+        <button>3m</button>
+        <button>1y</button>
+      </div>
+
+      <AreaChart data={formatData(chartData)} height={300} width={500}>
+        <Area type="monotone" dataKey="value" stroke="#8884d8" fill="#8884d8" />
+        <XAxis dataKey="date" />
+        <YAxis domain={["dataMin", "dataMax"]} />
+        <Tooltip />
+      </AreaChart>
+    </>
   );
 };
 
