ae6eed40c1bccb716f989930a7141c7c13df965c
oscar welton <oscarwelton@gmail.com>
2023-08-01 13:31:48 +0100

    sentiment popup added

--- frontend/src/widgets/Sentiment.jsx
+++ frontend/src/widgets/Sentiment.jsx
@@ -1,15 +1,28 @@
 import { scoreAlgorithm } from "../helpers/score-algorithm.js";
+import { useState } from "react";
 
 const Sentiment = (props) => {
+  const [isShown, setIsShown] = useState(false);
   const sentimentScore = scoreAlgorithm(props);
 
   return (
     <>
-      <h2>
-        Market Sentiment: {sentimentScore.status}
-        <button>i</button>
-      </h2>
-      <h2>Sentiment Score: {sentimentScore.score.toFixed(5)}</h2>
+      <div className="sentiment">
+        <h2>
+          Market Sentiment: {sentimentScore.status}
+          <button onClick={() => setIsShown(true)}>i</button>
+        </h2>
+        <h2>Sentiment Score: {sentimentScore.score.toFixed(5)}</h2>
+      </div>
+
+      {isShown && (
+        <div className="sentiment-info">
+          <button onClick={() => setIsShown(false)}>X</button>
+          <p>
+            The sentiment score is calculated by the number of positive words
+          </p>
+        </div>
+      )}
     </>
   );
 };
