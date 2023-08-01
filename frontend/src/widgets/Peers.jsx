72140a47a92d343219af4ccec974ad817ebd79f9
oscar welton <oscarwelton@gmail.com>
2023-08-01 11:34:30 +0100

    peers overflow

--- frontend/src/widgets/Peers.jsx
+++ frontend/src/widgets/Peers.jsx
@@ -3,10 +3,7 @@ import FlexBetween from "components/FlexBetween";
 import React, { useState } from "react";
 
 const Peers = ({ peers }) => {
-  // Changed function name to capitalized "Peers"
   const [isShown, setIsShown] = useState(-1);
-
-  console.log(peers[0], "hello world");
 
   return (
     <div className="peers">
@@ -20,20 +17,20 @@ const Peers = ({ peers }) => {
           <div className="peer-header">
             <p className="peer-symbol">{peer.symbol}</p>
             <p className="peer-quote"> {peer.quote.c}</p>
-            {isShown === index && (
-              <div className="peer-info">
-                <p>Opening: {peer.quote.o}</p>
-                <p>Previous Close: {peer.quote.pc}</p>
-                <p>High: {peer.quote.h}</p>
-                <p>Low: {peer.quote.l}</p>
-              </div>
-            )}
           </div>
 
           <FlexBetween style={{ color: getPercentageColor(peer.quote.d) }}>
             <p className="peer-data">{peer.quote.d}</p>
             <p>({peer.quote.dp.toFixed(2)}%)</p>
           </FlexBetween>
+          {isShown === index && (
+            <div className="peer-info">
+              <p>Opening: {peer.quote.o}</p>
+              <p>Previous Close: {peer.quote.pc}</p>
+              <p>High: {peer.quote.h}</p>
+              <p>Low: {peer.quote.l}</p>
+            </div>
+          )}
         </div>
       ))}
     </div>
